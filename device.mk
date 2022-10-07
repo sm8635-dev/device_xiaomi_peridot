312858c2f7b49ea88fef923d67dc3b2781bc0181
Dmitry Shmidt <dimitrysh@google.com>
2022-10-07 15:35:20 -0700

    peridot: init: Add option to use fscompress for F2FS
    Bug: 251226037
    Test: make BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE=f2fs TARGET_USERIMAGES_USE_F2FS=true PRODUCT_FS_COMPRESSION=1 -j24
    
    Change-Id: I19df57a92248c21bb65968fd287e0b58831353ad

--- device.mk
+++ device.mk
@@ -490,6 +490,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Userdata
+PRODUCT_FS_COMPRESSION := 1
+
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservice \
