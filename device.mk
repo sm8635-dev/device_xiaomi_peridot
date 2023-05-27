163a59608aa9943ae38eeac60117882f526f4ce1
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-05-27 09:27:30 +0200

    peridot: init: Import cgroup tuning from gs101
    Inspired by PixelOS-Devices/device_xiaomi_lisa-priv@936b04d
    
    Change-Id: I2069c256c1254752839869b42acf9a94
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -282,6 +282,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.qcom.factory.rc \
     init.peridot.rc \
+    init.sm8635.rc \
     init.qcom.rc \
     init.recovery.qcom.rc \
     init.target.rc
