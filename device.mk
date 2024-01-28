b40b575ca97c876eb5c452f5ffd576837f5e9d0a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-28 21:14:48 +0530

    peridot: Silence some spammy logging
    HWC and cit sensor logspam is so intense it bumps up logd's idle CPU
    usage up to over 3%. With this change, logs are much cleaner and logd
    idle CPU usage is reduced to under 1%, while still allowing error logs
    to pass through.
    
    Change-Id: I795651ccde4210717db5584b5fa7be792d3a66e5

--- device.mk
+++ device.mk
@@ -291,6 +291,27 @@ PRODUCT_ENABLE_UFFD_GC := true
 PRODUCT_PACKAGES += \
     KProfiles
 
+# Logging
+SPAMMY_LOG_TAGS := \
+    AiAiEcho \
+    MiStcImpl \
+    SDM \
+    SDM-histogram \
+    SRE \
+    WifiHAL \
+    cnss-daemon \
+    libsensor-displayalgo \
+    libsensor-parseRGB \
+    libsensor-ssccalapi \
+    sensors \
+    sensors-hal \
+    vendor.qti.hardware.display.composer-service \
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_VENDOR_PROPERTIES += \
+    $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=E)
+endif
+
 # Media
 PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/codec2/media_codecs_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_audio.xml
