08ce5babd4f32f349bd714943f52d4384be17485
John Galt <johngaltfirstrun@gmail.com>
2025-03-20 10:14:10 -0400

    peridot: Drop walt scheduler
    Change-Id: Ica74467254ab45e2bf3cd767f3b7ba83
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -506,8 +506,6 @@ function sdm660_sched_schedutil_dcvs() {
         echo 1401600 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/hispeed_freq
     fi
 
-    echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
-
     #if the kernel version >=4.19,set input_boost_freq accordingly
     if [ $KernelVersionA -ge 4 ] && [ $KernelVersionB -ge 19 ]; then
         echo "0:1401600" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
@@ -2376,7 +2374,6 @@ case "$target" in
             echo 86 > /proc/sys/kernel/sched_upmigrate
             echo 80 > /proc/sys/kernel/sched_group_downmigrate
             echo 90 > /proc/sys/kernel/sched_group_upmigrate
-            echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
             # Enable min frequency adjustment for big cluster
             if [ -f /sys/module/big_cluster_min_freq_adjust/parameters/min_freq_cluster ]; then
@@ -3047,7 +3044,6 @@ case "$target" in
       echo 90 > /proc/sys/kernel/sched_downmigrate
       echo 140 > /proc/sys/kernel/sched_group_upmigrate
       echo 120 > /proc/sys/kernel/sched_group_downmigrate
-      echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
       # configure governor settings for little cluster
       echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
@@ -3182,8 +3178,6 @@ case "$target" in
             echo 1401600 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/hispeed_freq
             echo 1056000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
 
-	    echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
-
             # sched_load_boost as -6 is equivalent to target load as 85. It is per cpu tunable.
             echo -6 >  /sys/devices/system/cpu/cpu0/sched_load_boost
             echo -6 >  /sys/devices/system/cpu/cpu1/sched_load_boost
@@ -3298,7 +3292,6 @@ case "$target" in
       # default sched up and down migrate values are 100 and 95
       echo 85 > /proc/sys/kernel/sched_group_downmigrate
       echo 100 > /proc/sys/kernel/sched_group_upmigrate
-      echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
       # colocation v3 settings
       echo 740000 > /proc/sys/kernel/sched_little_cluster_coloc_fmin_khz
@@ -3404,7 +3397,6 @@ case "$target" in
             # default sched up and down migrate values are 100 and 95
             echo 85 > /proc/sys/kernel/sched_group_downmigrate
             echo 100 > /proc/sys/kernel/sched_group_upmigrate
-            echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
             #colocation v3 settings
             echo 740000 > /proc/sys/kernel/sched_little_cluster_coloc_fmin_khz
@@ -3532,7 +3524,6 @@ case "$target" in
         echo 71 95 > /proc/sys/kernel/sched_upmigrate
         echo 85 > /proc/sys/kernel/sched_group_downmigrate
         echo 100 > /proc/sys/kernel/sched_group_upmigrate
-        echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
         echo 0 > /proc/sys/kernel/sched_coloc_busy_hyst_ns
         echo 0 > /proc/sys/kernel/sched_coloc_busy_hysteresis_enable_cpus
         echo 0 > /proc/sys/kernel/sched_coloc_busy_hyst_max_ms
@@ -3708,7 +3699,6 @@ case "$target" in
         # default sched up and down migrate values are 100 and 95
         echo 85 > /proc/sys/kernel/sched_group_downmigrate
         echo 100 > /proc/sys/kernel/sched_group_upmigrate
-        echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
         echo 0 > /proc/sys/kernel/sched_coloc_busy_hyst_ns
         echo 0 > /proc/sys/kernel/sched_coloc_busy_hysteresis_enable_cpus
         echo 0 > /proc/sys/kernel/sched_coloc_busy_hyst_max_ms
@@ -3880,8 +3870,6 @@ case "$target" in
             echo "0:1017600" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
             echo 80 > /sys/devices/system/cpu/cpu_boost/input_boost_ms
 
-	    echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
-
             # sched_load_boost as -6 is equivalent to target load as 85. It is per cpu tunable.
             echo -6 >  /sys/devices/system/cpu/cpu0/sched_load_boost
             echo -6 >  /sys/devices/system/cpu/cpu1/sched_load_boost
@@ -4050,7 +4038,6 @@ case "$target" in
     # default sched up and down migrate values are 100 and 95
     echo 85 > /proc/sys/kernel/sched_group_downmigrate
     echo 100 > /proc/sys/kernel/sched_group_upmigrate
-    echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
     #colocation v3 settings
     echo 740000 > /proc/sys/kernel/sched_little_cluster_coloc_fmin_khz
@@ -4719,7 +4706,6 @@ case "$target" in
 	echo 85 > /proc/sys/kernel/sched_downmigrate
 	echo 100 > /proc/sys/kernel/sched_group_upmigrate
 	echo 95 > /proc/sys/kernel/sched_group_downmigrate
-	echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
 	# configure governor settings for little cluster
 	echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
@@ -4855,7 +4841,6 @@ case "$target" in
 	echo 85 85 > /proc/sys/kernel/sched_downmigrate
 	echo 100 > /proc/sys/kernel/sched_group_upmigrate
 	echo 10 > /proc/sys/kernel/sched_group_downmigrate
-	echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
 	# cpuset parameters
 	echo 0-3 > /dev/cpuset/background/cpus
@@ -5031,7 +5016,6 @@ case "$target" in
 	echo 85 85 > /proc/sys/kernel/sched_downmigrate
 	echo 100 > /proc/sys/kernel/sched_group_upmigrate
 	echo 10 > /proc/sys/kernel/sched_group_downmigrate
-	echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
 	# cpuset parameters
 	echo 0-3 > /dev/cpuset/background/cpus
@@ -5269,7 +5253,6 @@ case "$target" in
 	echo 85 85 > /proc/sys/kernel/sched_downmigrate
 	echo 100 > /proc/sys/kernel/sched_group_upmigrate
 	echo 85 > /proc/sys/kernel/sched_group_downmigrate
-	echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 	echo 400000000 > /proc/sys/kernel/sched_coloc_downmigrate_ns
 
 	# cpuset parameters
