4a3a79894c767ec8573276e4642c63def892ac5b
zenin1504 <kayozzprojects@yandex.com>
2025-09-09 22:49:08 +0000

    peridot: Initial dolby atmos
    * From sony blobs without dolby vision
    
    Change-Id: Id6c4eeac11df4215adb58f43d2fb22ce
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- extract-files.py
+++ extract-files.py
@@ -19,6 +19,7 @@ from extract_utils.main import (
 
 namespace_imports = [
     'device/xiaomi/peridot',
+    'hardware/dolby',
     'hardware/qcom-caf/sm8650',
     'hardware/qcom-caf/wlan',
     'hardware/xiaomi',
