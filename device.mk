8283db177c6a1a0d3b9b558185f83018226a739a
LuK1337 <priv.luk@gmail.com>
2025-10-23 23:18:07 +0200

    peridot: Set PRODUCT_ENABLE_UFFD_GC to true
    This is not really necessary, but setting it prevents the build system
    from attempting to determine UFFD GC support on its own, during which
    it's triggering unnecessary kernel builds when compiling any app/jar.
    
    Change-Id: I8b8958f31c5437310ea27250447c8139e1684dcc

--- device.mk
+++ device.mk
@@ -280,6 +280,9 @@ $(call soong_config_set,lineage_health,fast_charge_value_none,0)
 $(call soong_config_set,lineage_health,fast_charge_value_fast_charge,1)
 $(call soong_config_set,lineage_health,fast_charge_value_super_fast_charge,2)
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := true
+
 # Media
 PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/codec2/media_codecs_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_audio.xml
