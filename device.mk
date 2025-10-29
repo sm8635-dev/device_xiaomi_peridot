7b940bcf6969eb95c4b01cc2cebd7d2252d01cc1
anky894 <ko282ko82@gmail.com>
2025-10-29 12:26:27 +0530

    peridot: Import perf boost configs from vendor
    Change-Id: I53a2f94833924a04fd34101919e781b4c8dead0b

--- device.mk
+++ device.mk
@@ -347,6 +347,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
+    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml \
     vendor/qcom/opensource/power/config/pineapple/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # Platform
