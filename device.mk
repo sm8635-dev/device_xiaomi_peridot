67f026226a85aedc33450b24447b25af7c35041b
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-24 03:08:29 +0800

    peridot: Update some soong config variables to bool type
    Change-Id: Ice4a8e2cc6d756a743116b46299cf833a0e78c57

--- device.mk
+++ device.mk
@@ -47,7 +47,7 @@ BOARD_SHIPPING_API_LEVEL := 34
 PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 
 # Audio
-$(call soong_config_set, android_hardware_audio, skip_speaker_layout_channel_mask_field, true)
+$(call soong_config_set_bool, android_hardware_audio, skip_speaker_layout_channel_mask_field, true)
 
 PRODUCT_PACKAGES += \
     android.hardware.audio.service
