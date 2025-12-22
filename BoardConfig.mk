5c6240f3219baa1d78a5f247fc5209d42b7c9545
zenin1504 <kayozzprojects@yandex.com>
2025-12-22 22:28:06 +0000

    peridot: Add lineage touch to FCM
    2025-12-22 20:54:49 - check_target_files_vintf.py - INFO : stderr:
    
    ERROR: files are incompatible.
    
    The following instances are in the device manifest but not specified in the
    framework compatibility matrix:
    
      vendor.lineage.touch.IHighTouchPollingRate/default (@1)
    
    Suggested fixes:
      1. Update deprecated HALs to the latest version.
      2. Check for any typos in the device manifest or framework compatibility
         matrices with FCM version >= 8.
      3. For new platform HALs, add them to any framework compatibility matrix
         with FCM version >= 8 where applicable.
      4. For device-specific HALs, add them to
         DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE or
         DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE.
      5. For `android.*` HALs using unexpected instance names, add the instance
         names to the AOSP framework compatibility matrices.
         (A regex wildcard may be used if the instance names are proprietary.)
    
    : Success
    
    Change-Id: I59b8a2ce1394498687c2b3904e66b5de
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,6 +69,7 @@ BOARD_USES_QCOM_HARDWARE := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/hidl/compatibility_matrix.device.xml \
+    $(DEVICE_PATH)/configs/hidl/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/yaap/config/device_framework_matrix.xml
 
