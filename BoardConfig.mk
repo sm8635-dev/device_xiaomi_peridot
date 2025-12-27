e40d14e3ce4bd1097e25efa4b216db5105e82ffd
zenin1504 <kayozzprojects@yandex.com>
2025-12-27 22:30:21 +0000

    peridot: Add `extra.config` for mountify support
    Change-Id: Ic4f9455b3e39431c91be16c2ff543b16
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,6 +108,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := TARGET_PRODUCT=$(PRODUCT_DEVICE)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8635
 TARGET_KERNEL_CONFIG := \
     gki_defconfig \
+    vendor/extra.config \
     vendor/pineapple_GKI.config \
     vendor/peridot_GKI.config
 
