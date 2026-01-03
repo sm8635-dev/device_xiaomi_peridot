5ed1632edc223fe5a4eba781ab5fb456c7644126
zenin1504 <kayozzprojects@yandex.com>
2026-01-03 23:26:59 +0100

    Revert "peridot: Drop Strongbox and switch to android.hardware.authsecret@1.0-service"
    This reverts commit 5e644ff250e30f027896144b7f74315b09c324e7.

--- device.mk
+++ device.mk
@@ -266,7 +266,7 @@ PRODUCT_COPY_FILES += \
 
 # Keymint
 PRODUCT_PACKAGES += \
-    android.hardware.authsecret@1.0-service
+    android.hardware.authsecret-service.nxp
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
