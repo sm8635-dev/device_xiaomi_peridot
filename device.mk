90b1430e2178a740888c16fb9f3b9a8e7480723f
zenin1504 <kayozzprojects@yandex.com>
2026-01-09 03:59:50 +0100

    peridot: Use proper dalvik heap for 8GB devices
    Change-Id: Ia12c5c8dd79544aa8a6f2c285bcd2fa5
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -150,12 +150,12 @@ PRODUCT_PACKAGES += \
 
 # Dalvik
 PRODUCT_VENDOR_PROPERTIES += \
-    dalvik.vm.heapstartsize?=16m \
-    dalvik.vm.heapgrowthlimit?=384m \
+    dalvik.vm.heapstartsize?=24m \
+    dalvik.vm.heapgrowthlimit?=256m \
     dalvik.vm.heapsize?=512m \
-    dalvik.vm.heaptargetutilization?=0.75 \
-    dalvik.vm.heapminfree?=512k \
-    dalvik.vm.heapmaxfree?=8m
+    dalvik.vm.heaptargetutilization?=0.46 \
+    dalvik.vm.heapminfree?=8m \
+    dalvik.vm.heapmaxfree?=48m
 
 # Display
 PRODUCT_PACKAGES += \
