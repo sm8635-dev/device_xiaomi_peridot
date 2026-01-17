4f8f6fbe9ed0e87c2d2b342b70b6d4d7e0ecfcd8
AdarshGrewal <adarshgrewal@gmail.com>
2026-01-18 02:09:42 +0530

    peridot: Build possible display components from source
    Change-Id: I8d13eb771a08b11834d5bdbed1803be8852a5320

--- device.mk
+++ device.mk
@@ -159,6 +159,11 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.demura-service
+
+PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer3-V1-ndk.vendor \
     vendor.qti.hardware.display.config-V11-ndk.vendor
 
