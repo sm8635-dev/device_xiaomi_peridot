fcc6408c5e545ba064de58bc03805355345da253
zenin1504 <kayozzprojects@yandex.com>
2026-01-21 18:52:40 +0800

    peridot: thermal: Modify our thermal configs properly
    * For some reason playing some games making phone throttling fast even when watching videos on 4k making it laggy instead modify thermal-normal.conf and correct some values
    
    * And also let's drop all .conf because it's unused now and we relying on thermal-normal.conf
    
    To test:
    - Make sure there's no abnormal issue on temperature
    - Check 4k video
    
    Change-Id: If34a93e3cf764a21aa3e99ea0e0c4e94
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -433,6 +433,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/thermal,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
