9e298d77a037b3848da15a291412756906504d2f
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-02-13 01:04:36 +0600

    peridot: Set target min supported frame rate of 60
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- device.mk
+++ device.mk
@@ -143,6 +143,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Display
 $(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
+$(call soong_config_set,surfaceflinger,frame_rate_category_min,60)
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
