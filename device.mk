471aa6edb2b4feb98d093076fb0088340ea7591e
zenin1504 <kayozzprojects@yandex.com>
2026-02-14 05:54:57 +0800

    peridot: Use correct values for `dalvik.vm`
    Change-Id: Ie920b8ef36c743e4a2400a776c8b2a56
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -150,12 +150,12 @@ OVERRIDE_DISABLE_DEXOPT_ALL := false
 
 # Dalvik
 PRODUCT_VENDOR_PROPERTIES += \
-    dalvik.vm.heapstartsize?=16m \
-    dalvik.vm.heapgrowthlimit?=384m \
-    dalvik.vm.heapsize?=512m \
-    dalvik.vm.heaptargetutilization?=0.75 \
-    dalvik.vm.heapminfree?=512k \
-    dalvik.vm.heapmaxfree?=8m
+    dalvik.vm.heapstartsize=24m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.46 \
+    dalvik.vm.heapminfree=8m \
+    dalvik.vm.heapmaxfree=48m
 
 # Display
 PRODUCT_PACKAGES += \
