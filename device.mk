6375f2f151e4c37a9aafc199c7b43a479f5e9265
zenin1504 <kayozzprojects@yandex.com>
2026-02-13 23:33:13 +0800

    peridot: Build KProfiles and label it properly
    Change-Id: I48c28b88fc864c1082c2f316e950e7ef
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -283,6 +283,10 @@ PRODUCT_COPY_FILES += \
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
+# KProfiles
+PRODUCT_PACKAGES += \
+    KProfiles
+
 # Media
 PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/codec2/media_codecs_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_audio.xml
