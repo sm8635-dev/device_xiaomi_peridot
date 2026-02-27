47bae6cea055c69f3a2d3936825fd0ebcda2598d
zenin1504 <kayozzprojects@yandex.com>
2026-02-28 07:32:57 +0800

    Revert "peridot: Add `vendor_yaap` fork for pixel prop utils update"
    * Already merge at https://github.com/yaap/vendor_yaap/pull/26/commits
    
    This reverts commit a639c9f201d276d78fdb3b9706cca657d6c82567.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -28,10 +28,5 @@ git fetch https://github.com/sm8635-dev/device_lineage_sepolicy sixteen
 git reset --hard FETCH_HEAD
 croot
 
-cd vendor/yaap
-git fetch https://github.com/sm8635-dev/vendor_yaap sixteen
-git reset --hard FETCH_HEAD
-croot
-
 rm -rf vendor/yaap/signing/keys
 git clone https://github.com/Neon-Duchamp/keys.git -b yaap-keys vendor/yaap/signing/keys
