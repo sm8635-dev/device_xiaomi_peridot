959e219e36abc2fb1c39f96f3551ed673fbce4f3
zenin1504 <kayozzprojects@yandex.com>
2026-03-04 15:54:41 +0800

    peridot: Allow unofficial build to use updater
    Change-Id: Iac77f792612e41ec855318050ea2c146
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,7 @@
+cd vendor/custom
+git fetch https://github.com/sm8635-dev/vendor_custom sixteen-qpr2
+git reset --hard FETCH_HEAD
+croot
+
 rm -rf vendor/lineage-priv/keys
 git clone https://github.com/Neon-Duchamp/keys.git -b old-keys vendor/lineage-priv/keys
