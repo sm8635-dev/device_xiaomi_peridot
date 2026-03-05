4e0df47016e86cf532348a628a304d644f8c4088
zenin1504 <kayozzprojects@yandex.com>
2026-03-06 00:49:02 +0800

    peridot: Drop sku props
    Change-Id: I7c097d2edd2c41e2ad16c317c189771f
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -391,12 +391,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.qspa-service \
     qspa_vendor.rc
 
-# SKU
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/properties/sku/odm_CN.prop:$(TARGET_COPY_OUT_ODM)/etc/odm_CN.prop \
-    $(LOCAL_PATH)/properties/sku/odm_GL.prop:$(TARGET_COPY_OUT_ODM)/etc/odm_GL.prop \
-    $(LOCAL_PATH)/properties/sku/odm_IN.prop:$(TARGET_COPY_OUT_ODM)/etc/odm_IN.prop
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
