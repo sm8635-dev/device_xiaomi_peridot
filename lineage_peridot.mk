5e48fe97d6f3e2a590511fb3f4cd4a8002fb1929
zenin1504 <kayozzprojects@yandex.com>
2026-03-13 18:09:54 +0800

    peridot: Drop rebundant build desc
    Change-Id: I3f548b2a9a7c4592bcdabbdda6d7b881
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- lineage_peridot.mk
+++ lineage_peridot.mk
@@ -17,17 +17,6 @@ $(call inherit-product, device/xiaomi/peridot/device.mk)
 PRODUCT_NAME := lineage_peridot
 PRODUCT_DEVICE := peridot
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := POCO
-PRODUCT_MODEL := 24069PC21G
-
-PRODUCT_SYSTEM_NAME := peridot_global
-PRODUCT_SYSTEM_DEVICE := peridot
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="peridot_global-user 15 AQ3A.240912.001 OS2.0.208.0.VNPMIXM release-keys" \
-    BuildFingerprint=POCO/peridot_global/peridot:15/AQ3A.240912.001/OS2.0.208.0.VNPMIXM:user/release-keys \
-    DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
-    DeviceProduct=$(PRODUCT_SYSTEM_NAME)
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
