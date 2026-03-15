e40e336e630b053f659b733db54ab13ecf109982
AdarshGrewal <adarshgrewal@gmail.com>
2026-03-15 16:47:03 +0530

    fixup! peridot: Build and configure xiaomi biometrics aidl
    Change-Id: Ic64323c178e09311933191137949d792fd552ae2

--- device.mk
+++ device.mk
@@ -169,7 +169,7 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
-$(call soong_config_set,XIAOMI_BIOMETRICS_FINGERPRINT,USE_NEW_IMPL,true)
+$(call soong_config_set_bool,XIAOMI_BIOMETRICS_FINGERPRINT,USE_NEW_IMPL,true)
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.xiaomi \
     libudfpshandler
