197af24bb7f4848a89cdc27a9e5b881b62850282
zenin1504 <kayozzprojects@yandex.com>
2026-03-16 11:19:13 +0800

    peridot: Refactor charging control
    Change-Id: Ia15d6cefe97447f5a573cbf8f9bd6573
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- parts/src/com/xiaomi/settings/battery/BatteryUtils.java
+++ parts/src/com/xiaomi/settings/battery/BatteryUtils.java
@@ -6,14 +6,33 @@
 
 package com.xiaomi.settings.battery;
 
-import android.os.SystemProperties;
+import android.util.Log;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class BatteryUtils {
+    private static final String TAG = "XiaomiBatteryUtils";
     public static final String PREF_CHARGING_CTRL = "charging_control";
     public static final String PREF_CHARGING_LIMIT = "charging_limit";
-    public static final String PROPERTY_SUSPEND = "sys.battery.suspend";
+    private static final String NODE_SUSPEND = "/sys/class/qcom-battery/input_suspend";
 
     public static void setChargingSuspend(boolean suspend) {
-        SystemProperties.set(PROPERTY_SUSPEND, suspend ? "1" : "0");
+        File file = new File(NODE_SUSPEND);
+        if (!file.exists()) return;
+
+        String value = suspend ? "1" : "0";
+
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            fos.write(value.getBytes(StandardCharsets.UTF_8));
+            fos.flush();
+            try {
+                fos.getFD().sync();
+            } catch (IOException ignored) {}
+            Log.d(TAG, "Successfully set " + NODE_SUSPEND + " to " + value);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to write to " + NODE_SUSPEND, e);
+        }
     }
 }
