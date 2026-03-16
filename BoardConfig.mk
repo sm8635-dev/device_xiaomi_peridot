cc20af00ec8899b0b16b59b9e4145e07349f3671
zenin1504 <kayozzprojects@yandex.com>
2026-03-16 07:02:30 +0100

    peridot: sepolicy: Fix neverallow error
    FAILED: //system/sepolicy:sepolicy_neverallows Neverallow check: sepolicy_neverallows                                                                 Outputs: out/soong/.intermediates/system/sepolicy/sepolicy_neverallows/android_common/policy                                                          Error: exited with code: 1                                                 Command: out/host/linux-x86/bin/checkpolicy -M -c 30 -o out/soong/.intermediates/system/sepolicy/sepolicy_neverallows/android_common/policy out/soong/.intermediates/system/sepolicy/sepolicy_neverallows.checkpolicy.conf/android_common/sepolicy_neverallows.checkpolicy.conf # hash of input list: 3dd72144341d878094bd899c418dfc99c9016b3792c41283bd1afc203eecd905
    Output:
    libsepol.report_failure: neverallow on line 929 of system/sepolicy/private/domain.te (or line 28438 of out/soong/.intermediates/system/sepolicy/sepolicy_neverallows.checkpolicy.conf/android_common/sepolicy_neverallows.checkpolicy.conf) violated by allow parts_app vendor_hal_displayfeature_xiaomi_service:service_manager { find };
    libsepol.check_assertions: 1 neverallow failures occurred                  Error while expanding policy
    
    Change-Id: I37627b385ec14847a7001181bc7a2a0d
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -238,6 +238,7 @@ include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Vendor security patch
 BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
