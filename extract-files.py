e8ebb013b168bc22471e0f2b01b8dbceeaf9d7aa
zenin1504 <kayozzprojects@yandex.com>
2026-03-27 03:34:12 +0000

    peridot: Properly patch mialgo and misr
    * This should fix unresolve symbol
    
    Change-Id: If8434f858e624fbf94aeaca3483c21ca
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- extract-files.py
+++ extract-files.py
@@ -320,8 +320,9 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libmialgo.so',
         'vendor/lib64/libmisr.so',
     ): blob_fixup()
-        .remove_needed(
-            'libopencl_loader.so'
+        .replace_needed(
+            'libopencl_loader.so',
+            'libperidot_ocl_shim.so',
     ),
 }  # fmt: skip
 
