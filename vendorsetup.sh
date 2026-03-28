8ee6b6a3b69312e1dde739ba66d2cfd7f1120a5f
zenin1504 <kayozzprojects@yandex.com>
2026-03-28 09:24:11 +0000

    peridot: Track our fork fwb
    * Needed for miui camera to work
    
    Change-Id: I1eba3cdbeac5488fa4eba07a5f32d8d2
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -28,5 +28,10 @@ git fetch https://github.com/sm8635-dev/device_lineage_sepolicy sixteen
 git reset --hard FETCH_HEAD
 croot
 
+cd frameworks/base
+git fetch https://github.com/sm8635-dev/frameworks_base sixteen
+git reset --hard FETCH_HEAD
+croot
+
 rm -rf vendor/yaap/signing/keys
 git clone https://github.com/Neon-Duchamp/keys.git -b yaap-keys vendor/yaap/signing/keys
