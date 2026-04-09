170f107a743b5eb5d2d5b42bc34461d212379dbf
zenin1504 <kayozzprojects@yandex.com>
2026-04-09 19:29:02 +0200

    Revert "peridot: Shallow fetch all the needed repos"
    This reverts commit 6fc646d213bc55251d27d963333e9a57d1e4675f.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,35 +1,35 @@
 cd hardware/qcom-caf/sm8650/audio/agm
-git fetch --depth 1 https://github.com/LineageOS/android_vendor_qcom_opensource_agm lineage-23.2-caf-sm8650
+git fetch https://github.com/LineageOS/android_vendor_qcom_opensource_agm lineage-23.2-caf-sm8650
 git reset --hard FETCH_HEAD
 croot
 
 cd hardware/qcom-caf/sm8650/audio/graphservices
-git fetch --depth 1 https://github.com/LineageOS/android_vendor_qcom_opensource_audioreach-graphservices lineage-23.2-caf-sm8650
+git fetch https://github.com/LineageOS/android_vendor_qcom_opensource_audioreach-graphservices lineage-23.2-caf-sm8650
 git reset --hard FETCH_HEAD
 croot
 
 cd hardware/qcom-caf/sm8650/audio/pal
-git fetch --depth 1 https://github.com/LineageOS/android_vendor_qcom_opensource_arpal-lx lineage-23.2-caf-sm8650
+git fetch https://github.com/LineageOS/android_vendor_qcom_opensource_arpal-lx lineage-23.2-caf-sm8650
 git reset --hard FETCH_HEAD
 croot
 
 cd hardware/qcom-caf/sm8650/audio/primary-hal
-git fetch --depth 1 https://github.com/LineageOS/android_hardware_qcom_audio-ar lineage-23.2-caf-sm8650
+git fetch https://github.com/LineageOS/android_hardware_qcom_audio-ar lineage-23.2-caf-sm8650
 git reset --hard FETCH_HEAD
 croot
 
 cd hardware/lineage/interfaces
-git fetch --depth 1 https://github.com/sm8635-dev/hardware_lineage_interfaces sixteen
+git fetch https://github.com/sm8635-dev/hardware_lineage_interfaces sixteen
 git reset --hard FETCH_HEAD
 croot
 
 cd device/lineage/sepolicy
-git fetch --depth 1 https://github.com/sm8635-dev/device_lineage_sepolicy sixteen
+git fetch https://github.com/sm8635-dev/device_lineage_sepolicy sixteen
 git reset --hard FETCH_HEAD
 croot
 
 cd frameworks/base
-git fetch --depth 1 https://github.com/sm8635-dev/frameworks_base sixteen
+git fetch https://github.com/sm8635-dev/frameworks_base sixteen
 git reset --hard FETCH_HEAD
 croot
 
