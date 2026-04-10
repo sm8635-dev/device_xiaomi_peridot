65842b2bfcd558796b6fc761feb45bf4abafbcde
zenin1504 <kayozzprojects@yandex.com>
2026-04-11 04:13:51 +0800

    peridot: Import device features on tree
    Change-Id: I0c932ab075454d878a90f80b20eb41c2
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -167,6 +167,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     RemovePackagesPeridot
 
+# Device-Features
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/device_features/peridot.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/peridot.xml
+
 # Enable whole-program R8 Java optimizations for SystemUI and system_server,
 # but also allow explicit overriding for testing and development.
 SYSTEM_OPTIMIZE_JAVA := true
