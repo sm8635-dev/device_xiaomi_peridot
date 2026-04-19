02bf2aa8d00f56485cc0b480f8d1a268ab046d8f
zenin1504 <kayozzprojects@yandex.com>
2026-04-19 22:07:57 +0200

    peridot: Add custom opendelta updater
    Change-Id: I6d166ca0ae994d0dacf6c969c14c4d11
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -399,7 +399,8 @@ PRODUCT_PACKAGES += \
     TelephonyOverlayPeridot \
     WifiOverlayPeridot \
     WifiOverlayPeridotPOCO \
-    WifiOverlayPeridotRedmi
+    WifiOverlayPeridotRedmi \
+    Xiaomi8635OpenDeltaOverlayVanilla
 
 PRODUCT_PACKAGES += \
     NcmTetheringOverlay
