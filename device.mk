5d2d9e23668b430dd9c117c702d7a61520732fb2
zenin1504 <kayozzprojects@yandex.com>
2026-04-27 02:50:46 +0800

    peridot: Guard PPU for gapps build
    Change-Id: I752c96dc0e414e719a9366d3443873fc
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -403,7 +403,6 @@ PRODUCT_PACKAGES += \
     ApertureOverlayPeridot \
     CarrierConfigOverlayPeridot \
     DeviceAsWebcamResTarget \
-    EmptyPropsOverlay \
     FrameworkOverlayPeridot \
     FastChargePeridot \
     LineageSDKOverlayPeridot \
@@ -423,6 +422,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     NcmTetheringOverlay
+
+ifeq ($(TARGET_BUILD_GAPPS),false)
+PRODUCT_PACKAGES += \
+    EmptyPropsOverlay
+endif
 
 # OpenDelta
 ifeq ($(TARGET_BUILD_GAPPS),true)
