47fe1671f21a8c7f74fe505f96f7c91e43bb9962
zenin1504 <kayozzprojects@yandex.com>
2026-05-07 04:22:04 +0200

    peridot: Redrop dexopt stuff
    Change-Id: Iab2fe5ec919944c2ac478e99406f0f55
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -178,31 +178,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/device_features/peridot.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/peridot.xml
 
-# Dexopt
-ifeq ($(TARGET_BUILD_VARIANT),user)
-PRODUCT_SYSTEM_PROPERTIES += \
-    pm.dexopt.post-boot=speed \
-    pm.dexopt.first-boot=speed \
-    pm.dexopt.boot-after-ota=speed-profile \
-    pm.dexopt.boot-after-mainline-update=speed \
-    pm.dexopt.install=speed-profile \
-    pm.dexopt.install-fast=speed \
-    pm.dexopt.install-bulk=speed-profile \
-    pm.dexopt.install-bulk-secondary=speed \
-    pm.dexopt.install-bulk-downgraded=speed \
-    pm.dexopt.install-bulk-secondary-downgraded=speed \
-    pm.dexopt.bg-dexopt=speed-profile \
-    pm.dexopt.ab-ota=speed-profile \
-    pm.dexopt.inactive=speed \
-    pm.dexopt.cmdline=speed \
-    pm.dexopt.first-use=speed-profile \
-    pm.dexopt.secondary=speed-profile \
-    pm.dexopt.shared=speed
-
-PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed
-OVERRIDE_DISABLE_DEXOPT_ALL := false
-endif
-
 # Debloat
 PRODUCT_PACKAGES += \
     RemovePackagesPeridot
