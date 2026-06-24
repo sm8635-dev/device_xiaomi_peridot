06007b08a0181e23df54626d2b4a5a83abc55e3b
ralph950412 <ralph950412@gmail.com>
2026-06-24 20:12:31 +0900

    peridot: Patch libwfdservice to load android.media.audio.common.types-V5-cpp
    Change-Id: Ia6730dcb44474a31b4b23877094de3f5

--- extract-files.py
+++ extract-files.py
@@ -82,6 +82,10 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so')
         .remove_needed('android.hidl.base@1.0.so'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed(
+            'android.media.audio.common.types-V4-cpp.so',
+            'android.media.audio.common.types-V5-cpp.so'
     'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
         .replace_needed(
             'libprotobuf-cpp-full.so',
