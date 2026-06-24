605d6d69d152f7f049b7cfa8469793cc880be7d7
binbin323 <bin@dhao.cc>
2026-06-25 01:32:14 +0900

    peridot: Patch wfdservice with libaudiobase
    Change-Id: I43e5de9e0dcd4a2b80b99a3e29939f76

--- extract-files.py
+++ extract-files.py
@@ -87,6 +87,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed(
             'android.media.audio.common.types-V4-cpp.so',
             'android.media.audio.common.types-V5-cpp.so'
+        .add_needed('libaudiobase.so')
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
