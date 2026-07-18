a2da4ae62e9c6714dd8305e9067a346b029bcc88
zenin1504 <kayozzprojects@yandex.com>
2026-07-18 18:39:38 +0800

    peridot: Comment out private keys
    Change-Id: Iad55a8936ccc4ab4a0c56de2c61112aa
    Signed-off-by: zenin1504 <me.zenin1504.dev@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-rm -rf vendor/lineage-priv/keys
-git clone https://github.com/sm8635-dev/signed_keys.git -b axion vendor/lineage-priv/keys
+#rm -rf vendor/lineage-priv/keys
+#git clone https://github.com/sm8635-dev/signed_keys.git -b axion vendor/lineage-priv/keys
